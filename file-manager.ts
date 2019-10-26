1f2cfaa3a4e031af3240528c2e075e68e6eeeb37
Craig Fay <craigfay1@gmail.com>
2019-10-26 18:19:21 -0500

    Add comments

--- file-manager.ts
+++ file-manager.ts
@@ -14,6 +14,10 @@ export function fileManager(dirpath) {
   }
 }
 
+/**
+ * Given the path to a directory, create a function that can read data from
+ * a single <table>.json files inside
+ */
 const tableReader = dirpath => async table => {
   const filename = path.join(dirpath, table) + ".json";
   // Try to read file
@@ -29,6 +33,10 @@ const tableReader = dirpath => async table => {
   return [data, null];
 }
 
+/**
+ * Given the path to a directory, create a function that can write data to
+ * a single <table>.json files inside
+ */
 const tableWriter = dirpath => async (table, data) => {
   const filename = path.join(dirpath, table) + ".json";
   const json = JSON.stringify(data, null, 2);
@@ -40,6 +48,10 @@ const tableWriter = dirpath => async (table, data) => {
   return [json, null];
 }
 
+/**
+ * Given the path to a directory, create a function that can read data from 
+ * all <table>.json files inside
+ */
 const allTableReader = dirpath => async () => {
   const files = await readDir(dirpath);
   const jsonFiles = files.filter(f => f.slice(-5) == '.json');
@@ -56,17 +68,20 @@ const allTableReader = dirpath => async () => {
   return [data, null];
 }
 
+// JSON.parse() without throwing
 function safeJsonParse(str) {
   try { return JSON.parse(str) }
   catch (e) { return e }
 }
 
+// New Error Type
 function tableNotExistsError(filename) {
   const e = new Error(`Expected ${filename} to exist`);
   e.name = 'TableNotExistsErr';
   return e;
 }
 
+// New Error Type
 function tableCorruptionError(filename) {
   const e = new Error(`Expected ${filename} to contain valid JSON`);
   e.name = 'TableCorruptionError';
