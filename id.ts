f5d1c11c8a6a2fd50fa7e875ad742ebe2b1513ff
Craig Fay <craigfay1@gmail.com>
2019-10-27 11:55:30 -0500

    Use default length for base36 ids

--- id.ts
+++ id.ts
@@ -11,7 +11,7 @@ export function hex() {
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const base36digits = '01234567890abcdefghijklmnopqrstuvwxyz'.split('');
 
-export function base36(length) {
+export function base36(length=16) {
     let str = '';
     for (let i = 0; i < length; i++) {
       str += base36digits[random(0, base36digits.length-1)]
