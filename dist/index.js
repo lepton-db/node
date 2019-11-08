0a3aee6e29c9fef0d0fdda634751bb6bf9a2d38f
Craig Fay <craigfay1@gmail.com>
2019-11-08 08:34:53 -0600

    Remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Package Entrypoint
-var database_1 = require("./database");
-exports.database = database_1.database;
-//# sourceMappingURL=index.js.map
--- dist/tests.js
+++
@@ -1,3 +0,0 @@
-var _a = require('waverunner'), run = _a.run, testSuite = _a.testSuite;
-run(testSuite(require('./create.test')));
-//# sourceMappingURL=tests.js.map
