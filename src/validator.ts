c75d166ebd5b08a2993d6aa362770b922af759b6
Craig Fay <craigfay1@gmail.com>
2019-11-13 01:12:17 -0600

    Cleanup

--- src/validator.ts
+++
@@ -1,7 +0,0 @@
-
-export const isNum = field => typeof field == 'number';
-export const isStr = field => typeof field == 'string';
-export const isBool = field => typeof field == 'boolean';
-export const isInt = field => Number.isInteger(field);
-export const isIso = field => field == new Date(field).toISOString();
-export const isEnum = (...values) => field => values.includes(field);
